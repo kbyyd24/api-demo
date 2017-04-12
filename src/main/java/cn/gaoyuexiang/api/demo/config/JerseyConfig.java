b20e9ab145e92f5b9d43bf2b3ee764aae9a8d8ad
gaoyx <melo@gaoyuexiang.cn>
2017-04-12 15:48:21 +0800

    modified jersey config

--- src/main/java/cn/gaoyuexiang/api/demo/config/JerseyConfig.java
+++ src/main/java/cn/gaoyuexiang/api/demo/config/JerseyConfig.java
@@ -1,5 +1,7 @@
 package cn.gaoyuexiang.api.demo.config;
 
+import cn.gaoyuexiang.api.demo.controllers.StudentController;
+import cn.gaoyuexiang.api.demo.model.Student;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +10,7 @@ public class JerseyConfig extends ResourceConfig{
 
   public JerseyConfig() {
     packages("cn.gaoyuexiang.api.demo.controllers");
+    register(StudentController.class);
   }
 
 }
