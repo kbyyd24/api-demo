c12a2173d50fdd99c0f087e365c6e4a552427015
gaoyx <melo@gaoyuexiang.cn>
2017-04-13 15:06:53 +0800

    add entity course

---
+++ src/main/java/cn/gaoyuexiang/api/demo/model/Course.java
@@ -0,0 +1,39 @@
+package cn.gaoyuexiang.api.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Course")
+public class Course {
+
+  @Id
+  private String id;
+
+  private String name;
+
+  public Course() {
+  }
+
+  public Course(String id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
